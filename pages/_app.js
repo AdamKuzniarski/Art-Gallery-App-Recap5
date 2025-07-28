ca82ff840ca518556394f64bc49d834497cd984b
AdamKuzniarski <adam.kuzniarski@gmx.de>
2025-07-28 10:44:31 +0200

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import GlobalStyle from "../styles";
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Link></Link>
       <GlobalStyle />
       <Component {...pageProps} />
     </>
