4dfd0ea86e1469760540785d9939f1e0a0f0f63e
AdamKuzniarski <adam.kuzniarski@gmx.de>
2025-07-28 10:53:30 +0200

    slightly changes - testinig

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,7 @@ import GlobalStyle from "../styles";
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <a></a>
+      
       <GlobalStyle />
       <Component {...pageProps} />
     </>
