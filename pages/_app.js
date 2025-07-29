1e040fde308cf83724f0b056ba3cfb049fe36225
AdamKuzniarski <adam.kuzniarski@gmx.de>
2025-07-29 17:14:47 +0200

    add use-local-storage-state library

--- pages/_app.js
+++ pages/_app.js
@@ -1,28 +1,14 @@
 import GlobalStyle from "../styles";
 import Layout from "@/components/Layout";
-import { useState, useEffect } from "react";
 import useSWR from "swr";
-
-function useLocalStorage(key, initialValue) {
-  const [value, setValue] = useState(() => {
-    if (typeof window === "undefined") return initialValue;
-    const stored = localStorage.getItem(key);
-    return stored ? JSON.parse(stored) : initialValue;
-  });
-
-  useEffect(() => {
-    localStorage.setItem(key, JSON.stringify(value));
-  }, [key, value]);
-
-  return [value, setValue];
-}
-
+import useLocalStorageState from "use-local-storage-state";
 
 const fetcher = (url) => fetch(url).then((response) => response.json());
 
-
 export default function App({ Component, pageProps }) {
-  const [favorites, setFavorites] = useLocalStorage("favorites", []);
+  const [favorites, setFavorites] = useLocalStorageState("favorites", {
+    defaultValue: [],
+  });
   //console.log("favorites:", favorites);
   function handleToggleFavorite(slug) {
     if (favorites.includes(slug)) {
